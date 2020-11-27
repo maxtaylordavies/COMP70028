b0fbf344793b8c4c1c4efdccbcdc94b014c1bd2c
Max Taylor-Davies <maxtaylordavies@gmail.com>
2020-11-27 03:46:44 +0000

    adds Network class to agent.py

--- coursework2/agent.py
+++ coursework2/agent.py
@@ -15,7 +15,30 @@
 ############################################################################
 ############################################################################
 
+import torch
 import numpy as np
+
+
+# The Network class inherits the torch.nn.Module class, which represents a neural network.
+class Network(torch.nn.Module):
+
+    # The class initialisation function. This takes as arguments the dimension of the network's input (i.e. the dimension of the state), and the dimension of the network's output (i.e. the dimension of the action).
+    def __init__(self, input_dimension, output_dimension):
+        # Call the initialisation function of the parent class.
+        super(Network, self).__init__()
+        # Define the network layers. This example network has two hidden layers, each with 100 units.
+        self.layer_1 = torch.nn.Linear(in_features=input_dimension, out_features=100)
+        self.layer_2 = torch.nn.Linear(in_features=100, out_features=100)
+        self.output_layer = torch.nn.Linear(
+            in_features=100, out_features=output_dimension
+        )
+
+    # Function which sends some input data through the network and returns the network's output. In this example, a ReLU activation function is used for both hidden layers, but the output layer has no activation function (it is just a linear layer).
+    def forward(self, input):
+        layer_1_output = torch.nn.functional.relu(self.layer_1(input))
+        layer_2_output = torch.nn.functional.relu(self.layer_2(layer_1_output))
+        output = self.output_layer(layer_2_output)
+        return output
 
 
 class Agent:
@@ -50,13 +73,13 @@ class Agent:
         self.action = action
         return action
 
-    # # Function to set the next state and distance, which resulted from applying action self.action at state self.state
-    # def set_next_state_and_distance(self, next_state, distance_to_goal):
-    #     # Convert the distance to a reward
-    #     reward = 1 - distance_to_goal
-    #     # Create a transition
-    #     transition = (self.state, self.action, reward, next_state)
-    #     # Now you can do something with this transition ...
+    # Function to set the next state and distance, which resulted from applying action self.action at state self.state
+    def set_next_state_and_distance(self, next_state, distance_to_goal):
+        # Convert the distance to a reward
+        reward = 1 - distance_to_goal
+        # Create a transition
+        transition = (self.state, self.action, reward, next_state)
+        # Now you can do something with this transition ...
 
     # Function to get the greedy action for a particular state
     def get_greedy_action(self, state):
